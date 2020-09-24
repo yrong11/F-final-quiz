24e5b33c2831bb50ebb0006872810a3eaa45cf4c
yurong <rong.yu@thoughtworks.com>
2020-09-24 16:59:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './src/App'
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./App.scss"
+
+class Hello extends React.Component {
+  render() {
+    return <div className="hello">hello world</div>
+  }
+}
+
+export default Hello;
